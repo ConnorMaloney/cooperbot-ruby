e072afe557a2cd2103891d5b3e92e05d37b4e5b9
Foopert <foopert@gmail.com>
2018-07-11 14:49:19 -0400

    ADD: can now use built in mac os TTS or eSpeak

--- cooper.rb
+++ cooper.rb
@@ -2,9 +2,22 @@ require 'pocketsphinx-ruby'
 require "espeak"
 include ESpeak
 
+# This uses the built in mac os
+system('say Hello World!')
+
 # Cooper introduces himself, then loops and repeats what you say
-Speech.new("Hello World, My name is Cooper.").speak
-Pocketsphinx::LiveSpeechRecognizer.new.recognize do |speech|
-  puts speech
-  Speech.new(speech).speak
-end
+puts "Hello World, My name is Cooper."
+# Speech.new("Hello World, My name is Cooper.").speak
+Speech.new("Hello World, My name is Cooper", pitch: 75).speak
+intro = ["Pleasure to meet your acquaintance.", "At your service.", "Let's roll.", "Primed and ready.", "Standing by."].sample
+puts intro
+Speech.new(intro, pitch: 75).speak
+# Speech.new("yes........ yes... no... no... yes... no... yes...", pitch: 75).speak
+
+
+
+
+# Pocketsphinx::LiveSpeechRecognizer.new.recognize do |speech|
+  # puts speech
+  # Speech.new(speech).speak
+# end
